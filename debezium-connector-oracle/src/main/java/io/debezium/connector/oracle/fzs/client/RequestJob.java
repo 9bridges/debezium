de9b820d781d62c3b7e3cddd645c4c78973ba484
fanyang168 <fanyang521@hotmail.com>
2022-11-29 17:43:54 +0800

    add head for new file | delete unused class

--- debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/fzs/client/RequestJob.java
+++ debezium-connector-oracle/src/main/java/io/debezium/connector/oracle/fzs/client/RequestJob.java
@@ -1,7 +1,10 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.connector.oracle.fzs.client;
 
-
-import cn.hutool.core.io.FileUtil;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
